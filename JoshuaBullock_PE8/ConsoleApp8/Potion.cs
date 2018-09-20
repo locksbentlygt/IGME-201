5c8b4f3a484171f4e40ab4e10e564a6c1f79666f
locksbentlygt <42787592+locksbentlygt@users.noreply.github.com>
2018-09-20 19:06:05 -0400

    adding PE8 for class

---
+++ JoshuaBullock_PE8/ConsoleApp8/Potion.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp8
+{
+    class Potion
+    {
+
+        string color;
+        double strength;
+        string name;
+        string usage;
+        double dosage;
+        
+        public Potion(string p_color, double p_strength, string p_name, string p_usage, double p_dosage)
+        {
+            color = p_color;
+            strength = p_strength;
+            name = p_name;
+            usage = p_usage;
+            dosage = p_dosage;
+        }
+
+        public Potion()
+        {
+            name = "Potion";
+            strength = 50;
+            color = "colorless";
+            usage = "to cast on a person";
+            dosage = 10;
+            
+        }
+
+        public void usePotion()
+        {
+            dosage = dosage -1;
+            Console.WriteLine("WHY DID YOU DRINK ONE!!!...now we only have {0} doses left!\n",dosage);
+        }
+
+        public void printPotion()
+        {
+            Console.WriteLine("The name of this potion is {0} \n its strength is {1}.\n its color is labeld as {2} \n you use it {3}.\n and its dosage is {4} \n" ,name,strength,color,usage,dosage);
+        }
+
+    }
+}
---
+++ JoshuaBullock_PE8/ConsoleApp8/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp8
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+           Potion myCustomPotion = new Potion("red",20,"hot death","to cause a hot death...duh",7);
+           Potion defaultPotion = new Potion();
+            myCustomPotion.printPotion();
+            defaultPotion.printPotion();
+            myCustomPotion.usePotion();
+            myCustomPotion.printPotion();
+            Console.ReadLine();
+
+
+        }
+    }
+}
